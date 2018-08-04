564ddb8af951778135e146e6fb18b4f2e37f1347
teamzz111 <aflr@outlook.com>
2018-08-04 15:30:36 -0500

    Angular Routing

--- FRONTEND/app/gui/src/app/app.module.ts
+++ FRONTEND/app/gui/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { LandingPageComponent } from './landing/landing-page.component';
+import { HomeComponent } from './home/home.component';
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'home', component: HomeComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HomeComponent
   ],
+  exports: [RouterModule],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(appRoutes)]
   ],
   providers: [],
   bootstrap: [AppComponent]
