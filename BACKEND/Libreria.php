ef3acd0c028684d8dd7a2af3d2bcfbf9c14c4252
Duvan Pedroza <duvan.a.pedroza.n@gmail.com>
2018-08-04 15:15:10 -0700

    cambio de lugar

--- BACKEND/Libreria.php
+++ BACKEND/Libreria.php
@@ -1,17 +1,21 @@
 <?php
   require 'phpqrcode/qrlib.php';
-  $dir = 'temp/'
+  $dir = 'temp/';
   if (!file_exists($dir))
     mkdir($dir);
-    
+
   $filename=$dir.'test.png';
+
+
+
+
 
   $tamanio = 10;
   $level= 'M';
   $frameSize=3;
   $contenido='Hola mundo';
 
-  QRcode::png($contenido, $filename, $level, $tamanio $frameSize);
+  QRcode::png($contenido, $filename, $level, $tamanio, $frameSize);
 
   echo '<img src="'.$filename.'" />';
 
